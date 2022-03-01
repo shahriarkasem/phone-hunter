c224489f9de88366d084a48ef24e0aa560c02ad3
Shahriar Kasem <shahriarkasem@gmail.com>
2022-03-01 17:58:39 +0600

    80% done

--- js/app.js
+++ js/app.js
@@ -47,5 +47,34 @@ function getDetails(dataId) {
     // console.log(dataId);
     fetch(`https://openapi.programming-hero.com/api/phone/${dataId}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayDetails(data.data))
+}
+const displayDetails = everyData => {
+    console.log(everyData);
+    const showDetails = document.getElementById('show-details');
+    showDetails.textContent = '';
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <div class="card">
+            <img src="${everyData.image}" class="card-img-top img-custom-details" alt="...">
+            <div class="card-body">
+                <h4 class="card-text">${everyData.name}</h4>
+                <h5 class="card-title">Release Date: ${everyData.releaseDate}</h5>
+                </br>
+                <h6 class="bold">Main Features</h6>
+                <p>Display Size: ${everyData.mainFeatures.displaySize}</P>
+                <p>Memory: ${everyData.mainFeatures.memory}</P>
+                <p>Chipset: ${everyData.chipset}</p>
+                <p><span class="bold">Sensors:</span> ${everyData.mainFeatures.sensors}</p>
+                <h6 class="bold">Others:</h6>
+                <p>Bluetooth: ${everyData.others.Bluetooth}</p>
+                <p>GPS: ${everyData.others.GPS}</p>
+                <p>NFC: ${everyData.others.NFC}</p>
+                <p>Radio: ${everyData.others.Radio}</p>
+                <p>USB: ${everyData.others.USB}</p>
+                <p>WLAN: ${everyData.others.WLAN}</p>
+            </div>
+        </div>
+    `
+    showDetails.appendChild(div);
 }
